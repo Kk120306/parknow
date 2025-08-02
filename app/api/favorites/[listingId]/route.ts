b8252ffcd15327a6849bc316423b401fad26aba4
Kai Kameyama <1203kk06@gmail.com>
2025-08-02 19:14:50 +0800

    changes to api

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -3,68 +3,64 @@ import { NextResponse } from "next/server";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import client from "@/app/lib/prismadb";
 
-interface IParams {
-    listingId: string; 
-}
-
 export async function POST(
-    req: Request,
-    { params }: { params: IParams }
+  req: Request,
+  { params }: { params: { listingId: string } }
 ) {
-    const currentUser = await getCurrentUser();
-    if (!currentUser) {
-        return NextResponse.error();
-    }
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
 
-    const { listingId } = params;  
+  const { listingId } = params;
 
-    if (!listingId || typeof listingId !== "string") {
-        throw new Error("Invalid ID");
-    }
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
 
-    const favoriteIds = [...(currentUser.favoriteIds || [])];
+  const favoriteIds = [...(currentUser.favoriteIds || [])];
 
-    favoriteIds.push(listingId);
+  favoriteIds.push(listingId);
 
-    const user = await client.user.update({
-        where: {
-            id: currentUser.id,
-        },
-        data: {
-            favoriteIds,
-        },
-    });
+  const user = await client.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
 
-    return NextResponse.json(user);
+  return NextResponse.json(user);
 }
 
 export async function DELETE(
-    req: Request,
-    { params }: { params: IParams }
+  req: Request,
+  { params }: { params: { listingId: string } }
 ) {
-    const currentUser = await getCurrentUser();
-    if (!currentUser) {
-        return NextResponse.error();
-    }
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
 
-    const { listingId } = params;  
+  const { listingId } = params;
 
-    if (!listingId || typeof listingId !== "string") {
-        throw new Error("Invalid ID");
-    }
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
 
-    let favoriteIds = [...(currentUser.favoriteIds || [])];
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
 
-    favoriteIds = favoriteIds.filter((id) => id !== listingId);
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
 
-    const user = await client.user.update({
-        where: {
-            id: currentUser.id,
-        },
-        data: {
-            favoriteIds,
-        },
-    });
+  const user = await client.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
 
-    return NextResponse.json(user);
+  return NextResponse.json(user);
 }
